d610da9dbe8b6325583b06da5f8a673fbb3d6a1d
Giga Tokhishvili <toxishvili123@gmail.com>
2023-11-19 22:52:23 +0400

    quick fix 3

--- src/components/CompletedForm.jsx
+++ src/components/CompletedForm.jsx
@@ -60,9 +60,10 @@ export default function CompletedForm(props) {
                                     </div>
                                         {COMPIMGS.map((carItem) => {
                                             const carSrc = carItem.split('-');
+                                            console.log(`/src/assets/${props.values[0].value.split(' ').join('')}`, carSrc[0])
                                             return (
                                                 <div  key={nanoid()}>
-                                                {`/assets/${props.values[0].value.split(' ').join('')}` === carSrc && <img className='car-img' src={carItem} alt="" />}
+                                                {`/src/assets/${props.values[0].value.split(' ').join('')}` === carSrc[0] && <img className='car-img' src={carItem} alt="" />}
                                                 </div>
                                             )
                                         })}
